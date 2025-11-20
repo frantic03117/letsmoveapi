c5e69410e425d83874d723b20f05f4daebf53415
= <=>
2025-11-20 10:15:03 +0530

    updated

--- routes/workoutRoute.js
+++ routes/workoutRoute.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
-const { getWorkouts, createWorkout } = require("../src/Controllers/WorkoutController");
+const { getWorkouts, createWorkout, updateWorkout, deleteWorkout } = require("../src/Controllers/WorkoutController");
 const { Auth } = require("../src/middleware/Auth");
+const Store = require("../src/middleware/Store");
 
 const router = Router();
 router.get('/', getWorkouts);
-router.post('/', Auth('Admin'), createWorkout);
+router.post('/', Auth('Admin'), Store('image').fields([
+    {
+        name: "banner",
+        maxCount : 1
+    }
+]), createWorkout);
+router.put('/update/:id', Auth('Admin'), Store('image').fields([
+    {
+        name: "banner",
+        maxCount : 1
+    }
+]), updateWorkout);
+router.delete('/delete/:id', Auth('Admin'), deleteWorkout);
 module.exports = router;
--- src/Controllers/WorkoutController.js
+++ src/Controllers/WorkoutController.js
@@ -4,6 +4,15 @@ const Workout = require("../Models/Workout");
 // CREATE WORKOUT
 exports.createWorkout = async (req, res) => {
     try {
+         const requiredFields = ["title", "description", "category"];
+        const emptyFields = requiredFields.filter((field) => !req.body[field]);
+        if (emptyFields.length > 0) {
+            return res.status(500).json({
+                success: 0,
+                message: "The following fields are required: " + emptyFields.join(", "),
+                fields: emptyFields,
+            });
+        }
         const { title, category, description, duration, calories, videos, level } = req.body;
 
         const banner = req.file ? req.file.path : null;
@@ -22,13 +31,13 @@ exports.createWorkout = async (req, res) => {
         });
 
         res.status(201).json({
-            success: true,
+            success: 1,
             message: "Workout created successfully",
             data: workout
         });
 
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        res.status(500).json({ success: 0, error: error.message });
     }
 };
 
@@ -37,9 +46,9 @@ exports.createWorkout = async (req, res) => {
 exports.getWorkouts = async (req, res) => {
     try {
         const workouts = await Workout.find().populate("category");
-        res.json({ success: true, data: workouts });
+        res.json({ success: 1, data: workouts });
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        res.status(500).json({ success: 0, error: error.message });
     }
 };
 
@@ -49,12 +58,12 @@ exports.getWorkout = async (req, res) => {
     try {
         const workout = await Workout.findById(req.params.id).populate("category");
 
-        if (!workout) return res.status(404).json({ success: false, message: "Workout not found" });
+        if (!workout) return res.status(404).json({ success: 0, message: "Workout not found" });
 
-        res.json({ success: true, data: workout });
+        res.json({ success: 1, data: workout });
 
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        res.status(500).json({ success: 0, error: error.message });
     }
 };
 
@@ -72,13 +81,13 @@ exports.updateWorkout = async (req, res) => {
         const updated = await Workout.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
         res.json({
-            success: true,
+            success: 1,
             message: "Workout updated successfully",
             data: updated
         });
 
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        res.status(500).json({ success: 0, error: error.message });
     }
 };
 
@@ -87,8 +96,8 @@ exports.updateWorkout = async (req, res) => {
 exports.deleteWorkout = async (req, res) => {
     try {
         await Workout.findByIdAndDelete(req.params.id);
-        res.json({ success: true, message: "Workout deleted successfully" });
+        res.json({ success: 1, message: "Workout deleted successfully" });
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        res.status(500).json({ success: 0, error: error.message });
     }
 };
