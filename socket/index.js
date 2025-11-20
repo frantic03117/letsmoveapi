d655dccaaa77256d60a02c4ef89e4de4a108a336
= <=>
2025-11-20 16:07:05 +0530

    ssl added

--- socket/index.js
+++ socket/index.js
@@ -5,10 +5,15 @@ const events = require('events');
 const fs = require('fs');
 const { WebSocketServer, WebSocket } = require("ws");
 require('dotenv').config();
+// const options = {
+//     key: fs.readFileSync('./ssl/private.pem'),
+//     cert: fs.readFileSync('./ssl/certificate.pem')
+// }
 const options = {
-    key: fs.readFileSync('./ssl/private.pem'),
-    cert: fs.readFileSync('./ssl/certificate.pem')
-}
+    key: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/privkey.pem', 'utf-8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/fullchain.pem', 'utf-8'),
+};
+
 const server = https.createServer(options, app);
 const wss = new WebSocketServer({ server });
 const emitter = new events.EventEmitter();
