e055d4849267a0f4c97adab33d418fe1a390993b
frantic03117 <shahrukh@franticpro.com>
2025-12-26 17:07:51 +0530

    updated

--- src/Models/UserActivity.js
+++ src/Models/UserActivity.js
@@ -17,32 +17,20 @@ const useractivityschema = new Schema({
 
 useractivityschema.pre("save", function (next) {
     if (this.start_time && this.end_time) {
-        // Validate time range
         if (this.end_time < this.start_time) {
             return next(new Error("end_time must be after start_time"));
         }
-
-        // Set activity_date (start of day)
         const date = new Date(this.start_time);
-        date.setHours(0, 0, 0, 0);
+        date.setUTCHours(0, 0, 0, 0);
         this.activity_date = date;
-
-        // Duration in minutes
         const durationMinutes =
             (this.end_time - this.start_time) / (1000 * 60);
-
         this.duration = Math.round(durationMinutes);
-
-        // Activity value in HOURS (2 decimal precision)
-        this.activity_value = Number(
-            (durationMinutes / 60).toFixed(2)
-        );
-
-        // Optional: auto-set unit
+        this.activity_value = Number((durationMinutes / 60).toFixed(2));
         this.activity_unit = "hours";
     }
-
     next();
 });
+
 
 module.exports = new model('UserActivity', useractivityschema);
